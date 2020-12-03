e3970c823aecd43df039f8b1114b94c6ca70e043
Laura Tammpere <laura.tammpere@clio.com>
2020-12-03 09:05:33 -0800

    ⭐ Solved Day 3 ⭐

--- 2020/03/solution.rb
+++ 2020/03/solution.rb
@@ -1,8 +1,8 @@
 class Solution
   
   def initialize(input)
-    @input = input.map { |s| s.strip }
-    @width = @input[0].length
+    @input  = input.map { |s| s.strip }
+    @width  = @input[0].length
     @height = @input.length
   end
   
@@ -11,6 +11,21 @@ class Solution
   end
 
   def solve_second_part!
+    slopes = [
+      [1, 1],
+      [3, 1],
+      [5, 1],
+      [7, 1],
+      [1, 2]
+    ]
+
+    total = 1
+    for slope in slopes
+      total *= count_trees_on_slope(0, 0, slope)
+    end
+
+    return total
+
   end
 
   def solve!
@@ -21,15 +36,16 @@ class Solution
   end
 
   private
-  def count_trees_on_slope(x, y)
-    return 0 if y == @height
+  def count_trees_on_slope(x, y, slope=[3, 1])
+    return 0 if y >= @height
 
     x = x % @width
-
+    
+    count = count_trees_on_slope(x + slope[0], y + slope[1], slope)
     if @input[y][x] == '#'
-      return 1 + count_trees_on_slope(x + 3, y + 1)
+      return 1 + count
     else
-      return count_trees_on_slope(x + 3, y + 1)
+      return count
     end
   end
 end
