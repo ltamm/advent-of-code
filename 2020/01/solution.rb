a6d4bd91a204ee5a1bcb31c93b2ee9afad40f2d0
Laura Tammpere <laura.tammpere@clio.com>
2020-12-03 16:27:55 -0800

    Cleaned up Day 1 Solution

--- 2020/01/solution.rb
+++ 2020/01/solution.rb
@@ -5,28 +5,19 @@ class Solution
   end
   
   def solve!
-    @input = @input.sort!
-
-    for n in @input do
-      # if this value was going to be part of the answer, we'd have found it already
-      @input.delete(n)
-      for m in @input do
+    while not @input.empty?
+      n = @input.shift
+      @input.each do |m|
         difference = 2020 - n - m
         if @input.include? difference
           return difference * n * m
         end
       end
     end
-
     return -1
   end
 end
 
 input_file = ARGV[0]
 input = File.readlines(input_file).map { |n| n.to_i }
-
-starting = Time.now
 puts Solution.new(input).solve!
-ending = Time.now
-elapsed = ending - starting
-puts elapsed
