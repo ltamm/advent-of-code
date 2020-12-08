c4176c5d28bc2d801c917cea81d413655598c56a
Laura Tammpere <laura.tammpere@clio.com>
2020-12-07 17:41:44 -0800

    ⭐ Solved Day 7 ⭐

--- 2020/07/solution.rb
+++ 2020/07/solution.rb
@@ -13,8 +13,8 @@ class Solution
       bag = mem[cursor]
       matches = @rules.scan(/^([\w\s]+) bags contain.*#{Regexp.quote(bag)}.*$/)
       matches.each do |match|
-        container_bag = match.first
-        mem << container_bag unless mem.include? container_bag
+        outer_bag = match.first
+        mem << outer_bag unless mem.include? outer_bag
       end
       cursor += 1
     end
@@ -23,6 +23,7 @@ class Solution
   end
 
   def solve_second_part
+    traverse_bag_wormhole('shiny gold') - 1
   end
 
   def solve
@@ -30,6 +31,23 @@ class Solution
     puts solve_first_part
     puts '---Solving Part 2---'
     puts solve_second_part
+  end
+
+  private
+
+  def traverse_bag_wormhole(bag)
+    # find the line describing the rule for this bag
+    innards = @rules.scan(/#{Regexp.quote(bag)} bags contain ([\s\w,]+)\.$/).first[0]
+
+    return 1 if innards == 'no other bags'
+
+    total = 1
+    innards.split(',').each do |innard|
+      innard = innard.strip
+      amt, desc = innard.scan(/(\d+) ([\w\s]+) bags?/).first
+      total += amt.to_i * traverse_bag_wormhole(desc)
+    end
+    total
   end
 end
 
